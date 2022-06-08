50ab9c6f9f081ef5bf6c29c23b71f9e36907febe
Dylan Scarlett <dylscar27@gmail.com>
2022-06-08 09:49:46 -0600

    Adds methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ friends = ["Cindy","Dylan","Ryan","Amy"]
 ages = [21, 23, 24, 26]
 temperatures = [98.6, 99.1, 98.1, 98.5]
 results = ["True","False","False","True"]
+# The friends.pop method will remove the last element of the array.
+friends.pop
+# The ages.push method will add an element to the end of the array.
+ages.push
+# The temperature.shift method will remove the first element of the array.
+temperature.shift
+# The results.unshift method will fill the array with elements that will start at the front.
+results.unshift
